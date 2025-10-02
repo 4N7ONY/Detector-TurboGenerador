acca27b8866abe965fcc1f581ba0d72df3c82b08
4N7ONY <amalcachilon@gmail.com>
2025-10-01 23:35:20 -0500

    Agregar archivos de proyecto.

---
+++ Detector-TurboGenerador/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Detector_TurboGenerador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Detector de incendio y homa del Turbo Generador");
+            
+            Random n = new Random();
+
+            while (true)
+            {
+                int temp = n.Next(70, 960);
+                Console.WriteLine("La temperatura del Turbo Generador es: " + temp + "°C");
+                if (temp < 190)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("La temperatura es normal...\n");
+                    Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine("                       _____________                       ");
+                    Console.WriteLine("                      /             \\                      ");
+                    Console.WriteLine("                     /   TURBINA     \\                     ");
+                    Console.WriteLine("       --------------                 --------------       ");
+                    Console.WriteLine("=======|  ()   ()   ()   ()   ()   ()   ()   ()    |=======");
+                    Console.WriteLine("       |-------------------------------------------|       ");
+                    Console.WriteLine("       |                                           |       ");
+                    Console.WriteLine("       |              GENERADOR ELECTRICO          |       ");
+                    Console.WriteLine("=======|  [::::::: ROTOR :::::::]  [::::::::]      |       ");
+                    Console.WriteLine("       |                                           |       ");
+                    Console.WriteLine("       |                                           |       ");
+                    Console.WriteLine("       ---------------------------------------------       ");
+                    Console.WriteLine("                   \\_________________/                     ");
+                    Console.ResetColor();
+                }
+                else if (temp >= 190 && temp < 210)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("¡Alerta! ¡Temperatura alta en el Turbo Generador!\n");
+                    Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine("                       _____________                       ");
+                    Console.WriteLine("                      /             \\                      ");
+                    Console.WriteLine("                     /   TURBINA     \\                     ");
+                    Console.WriteLine("       --------------                 --------------       ");
+                    Console.WriteLine("=======|  ()   ()   ()   ()   ()   ()   ()   ()    |=======");
+                    Console.WriteLine("       |-------------------------------------------|       ");
+                    Console.WriteLine("       |                                           |       ");
+                    Console.WriteLine("       |              GENERADOR ELECTRICO          |       ");
+                    Console.WriteLine("=======|  [::::::: ROTOR :::::::]  [::::::::]      |       ");
+                    Console.WriteLine("       |                                           |       ");
+                    Console.WriteLine("       |                                           |       ");
+                    Console.WriteLine("       ---------------------------------------------       ");
+                    Console.WriteLine("                   \\_________________/                     ");
+                    Console.ResetColor();
+                }
+                else if (temp >= 210 && temp < 300)
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("¡Alerta! ¡Humo dectado en el Turbo Generador.....!\n");
+                    Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine("                       _____________                       ");
+                    Console.WriteLine("                      /             \\                      ");
+                    Console.WriteLine("                     /   TURBINA     \\                     ");
+                    Console.WriteLine("       --------------                 --------------       ");
+                    Console.WriteLine("=======|  ()   ()   ()   ()   ()   ()   ()   ()    |=======");
+                    Console.WriteLine("       |-------------------------------------------|       ");
+                    Console.WriteLine("       |                                           |       ");
+                    Console.WriteLine("       |              GENERADOR ELECTRICO          |       ");
+                    Console.WriteLine("=======|  [::::::: ROTOR :::::::]  [::::::::]      |       ");
+                    Console.WriteLine("       |                                           |       ");
+                    Console.WriteLine("       |                                           |       ");
+                    Console.WriteLine("       ---------------------------------------------       ");
+                    Console.WriteLine("                   \\_________________/                     ");
+                    Console.ResetColor();
+                    Console.Beep(1000, 1000);
+                }
+                else if (temp >= 300)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red; 
+                    Console.WriteLine("¡Alerta! ¡Incendio detectado en el Turbo Generador!\n");
+                    Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine("                       _____________                       ");
+                    Console.WriteLine("                      /             \\                      ");
+                    Console.WriteLine("                     /   TURBINA     \\                     ");
+                    Console.WriteLine("       --------------                 --------------       ");
+                    Console.WriteLine("=======|  ()   ()   ()   ()   ()   ()   ()   ()    |=======");
+                    Console.WriteLine("       |-------------------------------------------|       ");
+                    Console.WriteLine("       |                                           |       ");
+                    Console.WriteLine("       |              GENERADOR ELECTRICO          |       ");
+                    Console.WriteLine("=======|  [::::::: ROTOR :::::::]  [::::::::]      |       ");
+                    Console.WriteLine("       |                                           |       ");
+                    Console.WriteLine("       |                                           |       ");
+                    Console.WriteLine("       ---------------------------------------------       ");
+                    Console.WriteLine("                   \\_________________/                     ");
+                    Console.ResetColor();
+                    Console.Beep(1500, 5300);
+                }
+                // Si el usuario presiona 'q', salir del bucle
+                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+                Thread.Sleep(2000); 
+            }
+        }
+    }
+}
---
+++ Detector-TurboGenerador/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Detector-TurboGenerador")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Detector-TurboGenerador")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("1c0ea763-32a5-4102-892d-820ccaa3e968")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
